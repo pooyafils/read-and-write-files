c782938b523964fa7c922d7ae9f5a45b876cf342
pooyafils <pooyafils@gmail.com>
2023-11-04 14:56:30 +0330

    adding aop for token

--- src/main/java/com/read/excle/propertiesfile/readfile/aop/ApiMonitoring.java
+++ src/main/java/com/read/excle/propertiesfile/readfile/aop/ApiMonitoring.java
@@ -5,16 +5,31 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
 @Aspect
 @Component
 public class ApiMonitoring {
 
     @Before("execution(* com.read.excle.propertiesfile.readfile.contoller.ReadAndWriteExcelFileController.*(..))")
-    public void check(JoinPoint joinPoint){
+    public void check(JoinPoint joinPoint) {
         Object[] signatureArgs = joinPoint.getArgs();
-        for (Object signatureArg: signatureArgs) {
+        for (Object signatureArg : signatureArgs) {
             System.out.println("Arg: " + signatureArg);
 
         }
+    }
+
+
+
+    @Before("execution(* com.read.excle.propertiesfile.readfile.contoller.ReadAndWriteExcelFileController.*(..))")
+    public void validateToken(JoinPoint joinPoint) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String token = request.getHeader("Authorization");
+        System.out.println(token);
+
     }
 }
